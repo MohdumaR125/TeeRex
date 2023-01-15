5a19f4991be8715e7dd4c0c4567783cf78bbf360
MohdumaR125 <umar231294@gmail.com>
2023-01-15 21:32:59 +0530

    filter updated

--- tee-rex-store/src/Components/Cards.jsx
+++ tee-rex-store/src/Components/Cards.jsx
@@ -4,8 +4,28 @@ import { useSelector } from "react-redux"
 
 const Cards = () =>{
     let data=useSelector(state=>state.displayData)
+    const [cartData,setCartData]=useState([]);
     
     useEffect(()=>{},[data])
+    
+    const addToCart = (e)=>{
+        const idx=e.target.id;
+        let itemNotPresent = cartData.map((el)=>{
+            if(el.id==idx){
+                return false
+            }
+        })
+        console.log(itemNotPresent)
+        if(itemNotPresent){
+            const item =data.find((el)=>{
+                if(el.id==idx){
+                    return el
+                }
+            })
+            setCartData([...cartData,item]);
+        }
+        console.log(cartData)
+    }
 
         return(
             <>
@@ -16,7 +36,7 @@ const Cards = () =>{
                     <h4>{el.name}</h4>
                     <div>
                         <h5>{el.price}</h5>
-                        <button>Add To Cart</button>
+                        <button id={el.id} onClick={addToCart}>Add To Cart</button>
                     </div>
                 </div>
             ))}
